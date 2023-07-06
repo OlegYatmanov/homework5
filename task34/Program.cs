f851151662a61ebc830d893205a63bc2f44d7c2d
Oleg Yatmanov <o_yatmanov@mail.ru>
2023-07-06 15:02:06 +0500

    Семинар 8 домашнее задание

---
+++ task34/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int InputNumber(string message)
+{
+    WriteLine(message);
+    return int.Parse(ReadLine());
+}
+int[] randomArray()
+{
+    int sizeArr = InputNumber("Введите размер массива: ");
+    int[] numbers = new int[sizeArr];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(99, 999);
+    }
+    return numbers;
+}
+int[] arr = randomArray();
+int result = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        result++;
+    }
+}
+Write("В массиве [{0}] ", string.Join(", ", arr));
+WriteLine($" количества чётных чисел равно: {result}");
---
+++ task38/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write ("Введите число элементов массива: ");
+int a = int.Parse(ReadLine());
+double [] numbers = new double [a];
+FillArray (numbers);
+PrintArray (numbers);
+
+double max = numbers [0];
+for (int i = 0; i < a; i++)
+    {
+        if (numbers[i] > max)
+         {
+            max = numbers[i];
+         }
+    }
+double min = numbers [0];
+for (int i = 0; i < a; i++)
+    {
+        if (numbers[i] < min)
+         {
+            min = numbers[i];
+         }
+    }
+double difference = max - min;
+WriteLine ("Разница между максимальными и минимальными элементами массива: "+difference);
+void FillArray (double [] array)
+  {
+    for (int i =0; i < array.Length; i++)
+    {
+       numbers [i] = new Random().Next(0,100);
+    }
+
+  }
+
+  void PrintArray (double [] array)
+  {
+    for (int i =0; i < array.Length; i++)
+    {
+       Write(numbers [i] + " ");
+    }
+    WriteLine();
+  }
